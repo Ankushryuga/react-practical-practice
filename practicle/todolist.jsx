3420140d420b638da9926626b6fdcd69087764b8
Ankush <104245302+Ankushryuga@users.noreply.github.com>
2025-05-29 13:22:02 +0530

    Update todolist.jsx

--- practicle/todolist.jsx
+++ practicle/todolist.jsx
@@ -1,41 +1,71 @@
 import { useState } from "react";
 const ToDoList = () => {
-  const [taskInformations, setTaskInformations] = useState([]);
-  const [inputValue, setInputValue] = useState("");
+  const [availableTask, setAvailableTask] = useState([]);
+  const [input, setInput] = useState("");
+  const [editTaskId, setEditTaskId] = useState(null);
+  const [editInput, setEditInput] = useState("");
 
   const handleAddNew = () => {
-    if (inputValue.trim() !== "") {
+    if (input.trim() !== "") {
       const newTask = {
         id: Date.now(),
-        taskName: inputValue,
+        taskName: input,
       };
-      setTaskInformations((prev) => [...prev, newTask]);
-      setInputValue("");
+      setAvailableTask((prev) => [...prev, newTask]);
+      setInput("");
     }
   };
-  const handleEdit = () => {};
 
-  const handleDelete = () => {};
+  const handleEdit = (id) => {
+    const taskToEdit = availableTask.find((task) => task.id === id);
+    setEditTaskId(id);
+    setEditInput(taskToEdit.taskName);
+  };
+
+  const handleSave = (id) => {
+    setAvailableTask((prev) =>
+      prev.map((task) =>
+        task.id === id ? { ...prev, taskName: editInput } : task
+      )
+    );
+    setEditTaskId(null);
+    setEditInput("");
+  };
+
+  const handleDelete = (id) => {
+    setAvailableTask((prev) => prev.filter((task) => task.id !== id));
+  };
+
   return (
-    <div className="todoList">
-      <div className="header">
-        <input
-          type="text"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        ></input>
-        <button onClick={handleAddNew}>Add New</button>
-        {taskInformations.length === 0 ? (
-          <p>No New ToDO Added</p>
-        ) : (
-          taskInformations.map((task) => (
-            <div key={task.id} className="maincontent">
-              <p>{task.taskName}</p>
-              <button>Edit</button>
-              <button>Delete</button>
-            </div>
-          ))
-        )}
+    <div>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></input>
+      <button onClick={handleAddNew}>Add New</button>
+      <div>
+        {availableTask.length !== 0
+          ? availableTask.map((task) => (
+              <div key={task.id} className="todocontent">
+                {editTaskId === task.id ? (
+                  <input
+                    type="text"
+                    value={editInput}
+                    onChange={(e) => setEditInput(e.target.value)}
+                  ></input>
+                ) : (
+                  <input type="text" value={task.taskName} disabled></input>
+                )}
+                {editTaskId === task.id ? (
+                  <button onClick={() => handleSave(task.id)}>Save</button>
+                ) : (
+                  <button onClick={() => handleEdit(task.id)}>Edit</button>
+                )}
+                <button onClick={() => handleDelete(task.id)}>Delete</button>
+              </div>
+            ))
+          : "No to do list available"}
       </div>
     </div>
   );
